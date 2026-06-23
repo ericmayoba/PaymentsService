4429992c0a9f730e0d20fd4055432e6470662d35
Eric Ramirez <ericmayoba@gmail.com>
2026-06-23 15:50:10 -0400

    fix: change Balance type on wallet class  from double to decimal

--- src/PaymentsService.Domain/Entities/Wallet.cs
+++ src/PaymentsService.Domain/Entities/Wallet.cs
@@ -5,7 +5,7 @@ public class Wallet
     public int Id { get; set; }
     public string DocumentId { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
-    public double Balance { get; set; }
+    public decimal Balance { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
